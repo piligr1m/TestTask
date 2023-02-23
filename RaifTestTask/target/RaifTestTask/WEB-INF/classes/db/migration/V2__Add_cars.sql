7e790c91c13c1b990819c842202bc63022cb0166
Martynov Stanislav <stanislav.m.martynov@gmail.com>
2023-02-23 20:25:29 +0300

    Add project

---
+++ RaifTestTask/target/RaifTestTask/WEB-INF/classes/db/migration/V2__Add_cars.sql
@@ -0,0 +1,7 @@
+INSERT INTO cars (color, horsepower, quantity) VALUES
+                                                   ('green', '50', '10'),
+                                                   ('green', '55', '5'),
+                                                   ('green', '70', '2'),
+                                                   ('pink', '60', '5'),
+                                                   ('pink', '65', '7'),
+                                                   ('black', '100', '9')
