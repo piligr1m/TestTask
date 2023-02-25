26e4bc9ca9b91d34412308d18254f916142c7568
Martynov Stanislav <stanislav.m.martynov@gmail.com>
2023-02-25 11:51:01 +0300

    Delete RaifTestTask/target directory

--- RaifTestTask/target/classes/db/migration/V2__Add_cars.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO cars (color, horsepower, quantity) VALUES
-                                                   ('green', '50', '10'),
-                                                   ('green', '55', '5'),
-                                                   ('green', '70', '2'),
-                                                   ('green', '75', '3'),
-                                                   ('green', '80', '4'),
-                                                   ('pink', '60', '5'),
-                                                   ('pink', '65', '7'),
-                                                   ('black', '100', '9')
